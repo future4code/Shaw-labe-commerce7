27e623b8b4c6c560d23b06b17a657fd39551950e
Rodrigo Vieira Batista dos Santos <rodrigo.v.santos89@gmail.com>
2022-03-31 10:07:48 -0300

    Parte de Products

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,2 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import ProductsCart from "./ProductsCart";
+
+class Products extends React.Component {
+    render(){ 
+      
+      return (
+      <>
+            
+          <div>
+              <h2>Quantidade de produtos: </h2>
+
+              <h2>Ordenação:</h2>
+
+              <select>
+                <label>Ordenação</label>
+                <option value="Crescente" selected>Crescente</option>
+                <option value="Decrescente">Decrescente</option>
+              </select>
+
+          </div>
+
+          <ProductsCart/>
+
+      </>
+      );
+    }
+   
+  }
+  export default Products;
