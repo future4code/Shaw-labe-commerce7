97f69282fe1d711fb49b53c6ef4d11a99ff3484d
Lis Ribeiro <lisribeiro197@gmail.com>
2022-03-31 11:03:16 -0300

    Filter e products atualizados

--- src/App.js
+++ src/App.js
@@ -6,18 +6,46 @@ import Products from "./Components/Products/Products"
 import ShoppingCart from "./Components/ShoppingCart/ShoppingCart"
 import ShoppingCartIten from "./Components/ShoppingCart/ShoppingCartIten"
 
+const AppContainer = styled.div`
+ display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  padding: 16px;
+  gap: 8px;
+`
 
+class App extends React.Component {
+  state = {
+    valorMinimo: 0,
+    valorMaximo: 0,
+    buscarPorNome: "",
+  }
 
-export class App extends React.Component {
-  render(){ 
-    
+  onChangeValorMinimo = (event) => {
+    this.setState({ valorMinimo: event.target.value })
+  }
+
+  onChangeValorMaximo = (event) => {
+    this.setState({ valorMaximo: event.target.value })
+  }
+
+  onChangeBuscarPorNome = (event) => {
+    this.setState({ buscarPorNome: event.target.value })
+  }
+
+  render() {
     return (
-    <div >
-      
-    </div>
-  );
-}
- 
+      <AppContainer>
+        <Filters
+          valorMinimo={this.state.valorMinimo}
+          valorMaximo={this.state.valorMaximo}
+          buscarPorNome={this.state.buscarPorNome}
+          onChangeValorMinimo={this.onChangeValorMinimo}
+          onChangeValorMaximo={this.onChangeValorMaximo}
+          onChangeBuscarPorNome={this.onChangeBuscarPorNome}
+        />
+      </AppContainer>
+    )
+  }
 }
 
-
+export default App;
